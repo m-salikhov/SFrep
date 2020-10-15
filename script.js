11b32e19794d672f3acc0f70f69153e859795ae3
Maks <andvary@inbox.ru>
2020-10-15 22:36:04 +0300

    init commit 1

---
+++ script.js
@@ -0,0 +1,42 @@
+'use strict'
+
+// console.log('Hello, World!');
+// console.log(5 + 8);
+
+// let userName = 'petya';
+// console.log(userName);
+
+// userName = 'Misha';
+// console.log(userName);
+
+// let defaultName = 'Maks';
+// userName = defaultName ;
+
+// console.log(userName);
+
+// const block_size = 50 + 30;
+// console.log(block_size);
+
+// console.log(typeof defaultName);
+
+// let myAdress = {
+//     town: 'Moscow',
+//     street: 'Beringov',
+//     house: 42
+// }
+// console.log(myAdress);
+// // alert(typeof (myAdress));
+
+// userName = Boolean(userName);
+// console.log(userName);
+
+let anyNum = prompt('Your number');
+anyNum = +anyNum;
+
+if (isNaN(anyNum)) {
+    console.log('not a number');
+} else if (anyNum % 2 == 0) {
+    console.log (anyNum + ' - chetnoe')
+} else if (anyNum % 2 !== 0) {
+    console.log (anyNum + ' - ne chetnoe')
+}
